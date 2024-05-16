94dbcbb69fcb3047f9f28b76304a5387d36bddc2
Luan <luanlima747@gmail.com>
2024-05-15 21:58:17 -0300

    feito alteração da forma como a matriz recebe os dados

--- desafio_telefone.py
+++ desafio_telefone.py
@@ -1,9 +1,13 @@
-def relatorio(matriz_cliente,tipo_assinatura):
+def relatorio(matriz_cliente, tipo_assinatura):
     print("RELATORIO")
     for cliente in matriz_cliente:
-        valor_conta = calculo_conta(cliente[2],cliente[3],tipo_assinatura)
-        print(f"Nome: {cliente[0]}, {cliente[1]}, Tipo {cliente[2]}, Minutos: {cliente[3]}, Conta = R$ {valor_conta:.2f}")
-def calculo_conta(tipo_conta,minutos_utilizados,tipo_assinatura):
+        valor_conta = calculo_conta(cliente[2], cliente[3], tipo_assinatura)
+        print(
+            f"Nome: {cliente[0]}, {cliente[1]}, Tipo {cliente[2]}, Minutos: {cliente[3]}, Conta = R$ {valor_conta:.2f}"
+        )
+
+
+def calculo_conta(tipo_conta, minutos_utilizados, tipo_assinatura):
     # se minutos consumidos <- 90 == preço Basico
     # se nao sera // tipo_conta + (minutos consumido - 90 * excedente) == valor da conta.
     tarifa = tipo_assinatura[tipo_conta]
@@ -15,27 +19,23 @@ def calculo_conta(tipo_conta,minutos_utilizados,tipo_assinatura):
         valor_conta = tarifa_fixa + (minutos_utilizados - 90) * tarifa_excesso
     return valor_conta
 
+
 quantidade_cliente = int(input("Informe a quantidade de clientes: "))
 
 dados_clientes = []
 tipo_assinatura = [
-    [25.5, 0.10], #TIPO 0: [TARIFA_FIXA, TARIFA_EXCESSO]
-    [35.0, 0.12], #TIPO 1: [TARIFA_FIXA, TARIFA_EXCESSO]
-    [60.0, 0.15]  #TIPO 2: [TARIFA_FIXA, TARIFA_EXCESSO]
+    [25.5, 0.10],  # TIPO 0: [TARIFA_FIXA, TARIFA_EXCESSO]
+    [35.0, 0.12],  # TIPO 1: [TARIFA_FIXA, TARIFA_EXCESSO]
+    [60.0, 0.15],  # TIPO 2: [TARIFA_FIXA, TARIFA_EXCESSO]
 ]
 for _ in range(quantidade_cliente):
-    linha = []
     nome = input("Nome: ")
     telefone = input("Telefone: ")
-    tipo_conta = int(input("Tipo: "))
+    tipo_conta = int(input("Tipo de conta (0,1 ou 2): "))
     minutos = int(input("Minutos: "))
 
-    linha.append(nome)
-    linha.append(telefone)
-    linha.append(tipo_conta)
-    linha.append(minutos)
-
+    linha = [nome, telefone, tipo_conta, minutos]
     dados_clientes.append(linha)
 
 
-relatorio(dados_clientes,tipo_assinatura)
+relatorio(dados_clientes, tipo_assinatura)
