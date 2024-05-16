9e770c5e0cf4d9f9ce8d9378b8a55810825e0bda
Luan <luanlima747@gmail.com>
2024-05-15 22:14:25 -0300

    Adicionado uma função para cadastrar cliente separando do Main, deixando o codigo mais organizado, e tambem adicionado uma condição While no tipo de conta, para que se o usuario digitar um numero != 0,1 ou 2 ele retorne o erro

--- desafio_telefone.py
+++ desafio_telefone.py
@@ -5,8 +5,6 @@ def relatorio(matriz_cliente, tipo_assinatura):
         print(
             f"Nome: {cliente[0]}, {cliente[1]}, Tipo {cliente[2]}, Minutos: {cliente[3]}, Conta = R$ {valor_conta:.2f}"
         )
-
-
 def calculo_conta(tipo_conta, minutos_utilizados, tipo_assinatura):
     # se minutos consumidos <- 90 == preço Basico
     # se nao sera // tipo_conta + (minutos consumido - 90 * excedente) == valor da conta.
@@ -18,24 +16,33 @@ def calculo_conta(tipo_conta, minutos_utilizados, tipo_assinatura):
     else:
         valor_conta = tarifa_fixa + (minutos_utilizados - 90) * tarifa_excesso
     return valor_conta
+def cadastrar_clientes(quantidade_clientes):
+    dados_clientes = []
+    for i in range(quantidade_clientes):
+        print(f"Dados do {i+1}º Cliente: ")
+        nome = input("Nome: ")
+        telefone = input("Telefone: ")
+        tipo_conta = int(input("Tipo de conta (0,1 ou 2): "))
+        
+        while tipo_conta != 0 and tipo_conta != 1 and tipo_conta != 2:
+            print("Tipo da conta invalido, Digite ( 0,1 ou 2: )")
+            tipo_conta = input()
 
+        minutos = int(input("Minutos: "))
+        print()
+
+        linha = [nome, telefone, tipo_conta, minutos]
+        dados_clientes.append(linha)
+    return dados_clientes
 
 quantidade_cliente = int(input("Informe a quantidade de clientes: "))
 
-dados_clientes = []
 tipo_assinatura = [
     [25.5, 0.10],  # TIPO 0: [TARIFA_FIXA, TARIFA_EXCESSO]
     [35.0, 0.12],  # TIPO 1: [TARIFA_FIXA, TARIFA_EXCESSO]
     [60.0, 0.15],  # TIPO 2: [TARIFA_FIXA, TARIFA_EXCESSO]
 ]
-for _ in range(quantidade_cliente):
-    nome = input("Nome: ")
-    telefone = input("Telefone: ")
-    tipo_conta = int(input("Tipo de conta (0,1 ou 2): "))
-    minutos = int(input("Minutos: "))
-
-    linha = [nome, telefone, tipo_conta, minutos]
-    dados_clientes.append(linha)
 
 
-relatorio(dados_clientes, tipo_assinatura)
+clientes_cadastrados = cadastrar_clientes(quantidade_cliente)
+relatorio(clientes_cadastrados, tipo_assinatura)
