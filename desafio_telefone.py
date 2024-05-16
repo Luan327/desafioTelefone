334574e2256fcbeaf88eeb216e1dca8665afa4ca
Luan <luanlima747@gmail.com>
2024-05-15 21:05:37 -0300

    adicionado relatorio

--- desafio_telefone.py
+++ desafio_telefone.py
@@ -1,3 +1,10 @@
+def relatorio(matriz_cliente):
+    print("RELATORIO")
+    for cliente in matriz_cliente:
+        print(f"Nome: {cliente[0]},{cliente[1]},Tipo {cliente[2]}, Minutos: {cliente[3]}, Conta = R$ {0}")
+
+
+
 quantidade_cliente = int(input("Informe a quantidade de clientes: "))
 
 dados_clientes = []
@@ -5,16 +12,16 @@ dados_clientes = []
 for _ in range(quantidade_cliente):
     linha = []
     nome = input("Nome: ")
+    telefone = input("Telefone: ")
     tipo_conta = int(input("Tipo: "))
     minutos = int(input("Minutos: "))
-    
-    
+
     linha.append(nome)
+    linha.append(telefone)
     linha.append(tipo_conta)
     linha.append(minutos)
 
     dados_clientes.append(linha)
 
-for dados in dados_clientes:
-    print(dados)
+relatorio(dados_clientes)
 
